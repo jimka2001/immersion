b55a9261279c2764fabfb0ea1344884ab1c09ccd
Jim Newton <jimka2001@users.noreply.github.com>
2024-10-25 15:45:05 +0200

    changed some function names, updated some comments

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from math import sqrt
 
 
-def compute_cubic_coefficients_from_roots(r1, r2, r3):
-    """Return a tuple of 4 numbers which represent
+def cubic_coefficients_from_roots(r1, r2, r3):
+    """Return an array of 4 numbers which represent
     coefficients of a polynomial having r1, r2, and r3
-    as roots.  if the tuple returned is (a,b,c,d),
+    as roots.  if the array returned is [a,b,c,d],
     then the polynomial is ax^3 + bx^2 + cx + d"""
     return [1,
             -r1 + r2 + r3,
@@ -12,7 +12,7 @@ def compute_cubic_coefficients_from_roots(r1, r2, r3):
             -r1 * r2 * r3]
 
 
-def compute_quadratic_roots_from_coefficients(a, b, c):
+def quadratic_roots_from_coefficients(a, b, c):
     epsilon = 0.00001
     discriminant = b * b - 4 * a * c
 
@@ -46,23 +46,23 @@ def find_window(lower, upper, f):
         return find_window(lower - delta, upper + delta)
 
 
-def compute_cubic_roots_from_coefficients(a, b, c, d):
+def cubic_roots_from_coefficients(a, b, c, d):
     """Given the coefficients of a cubic polynomial,
     compute the roots if possible.
     semantics of the coefficients are: ax^3 + bx^2 + cx + d
     """
     if a < 0:
-        return compute_cubic_roots_from_coefficients(-a, -b, -c, -d)
+        return cubic_roots_from_coefficients(-a, -b, -c, -d)
 
     def f(x):
         return a * x ** 3 + b * x ** 2 + c * x + d
 
     lower, upper = find_window(-1.0, 1.0, f)
-    r1 = find_root_in_range(lower, upper, f)
+    r1 = find_root_in_range(lower, upper, f, 0.00001)
     A = a
     B = b + A * r1
     C = c + B * r1
-    r2r3 = compute_quadratic_roots_from_coefficients(A, B, C)
+    r2r3 = quadratic_roots_from_coefficients(A, B, C)
     if r2r3:
         r2, r3 = r2r3
         return [r1, r2, r3]
