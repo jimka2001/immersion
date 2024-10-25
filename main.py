230f62145287ee2e710def2ae701e24803b16766
Jim Newton <jimka2001@users.noreply.github.com>
2024-10-25 14:35:47 +0200

    white space

--- main.py
+++ main.py
@@ -4,16 +4,14 @@ def compute_cubic_coefficients_from_roots(r1, r2, r3):
     as roots.  if the tuple returned is (a,b,c,d),
     then the polynomial is ax^3 + bx^2 + cx + d"""
     return (1,
-            -r1+r2+r3,
-            r1*r2 + r2*r3 + r1*r3,
-            -r1*r2*r3)
+            -r1 + r2 + r3,
+            r1 * r2 + r2 * r3 + r1 * r3,
+            -r1 * r2 * r3)
 
-def compute_roots_from_coefficients(a,b,c,d):
-    """Given the coeficients of a cubic polynomial,
+
+def compute_roots_from_coefficients(a, b, c, d):
+    """Given the coefficients of a cubic polynomial,
     compute the roots if possible.
     semantics of the coefficients are: ax^3 + bx^2 + cx + d
     """
     pass
-
-
-
