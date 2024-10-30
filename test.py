bb58ade1b63d86c08ac96b3854ce36632a4b6472
Jim Newton <jnewton@lrde.epita.fr>
2024-10-30 09:11:55 +0100

    debugging

--- test.py
+++ test.py
@@ -17,7 +17,13 @@ print([f(r1), f(r2), f(r3), f(r4)])
 
 for r1 in range(-5, 5):
     for r2 in range(r1, 5):
+        a, b, c = find_quadratic_coefficients(float(r1), float(r2))
+        print([r1, r2, a, b, c])
+        roots = find_quadratic_roots(a, b, c)
         for r3 in range(r2, 5):
+            a, b, c, d = find_cubic_coefficients(float(r1), float(r2), float(r3))
+            print([r1, r2, r3, a, b, c, d])
+            roots = find_cubic_roots(a, b, c, d)
             for r4 in range(r3, 5):
                 a, b, c, d, e = find_quartic_coefficients(float(r1), float(r2), float(r3), float(r4))
                 print([r1, r2, r3, r4, a, b, c, d, e])
