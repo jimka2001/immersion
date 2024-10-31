9f6bea31b4659c60cae48b3a83c89d7009c0c0d3
Jim Newton <jnewton@lrde.epita.fr>
2024-10-31 08:40:21 +0100

    removing unused code

--- main.py
+++ main.py
@@ -50,7 +50,6 @@ def find_quartic_coefficients(r1, r2, r3, r4):
 def find_quadratic_roots(a, b, c):
     epsilon = 0.001
     discriminant = b * b - 4 * a * c
-    print(f"{discriminant=}")
     if discriminant > epsilon:
         return [(-b + sqrt(discriminant)) / (2 * a),
                 (-b - sqrt(discriminant)) / (2 * a)]
@@ -64,10 +63,7 @@ def find_quadratic_roots(a, b, c):
 def search_root_left(lower, upper, f, epsilon):
     """search to the left of lower to find an x value for which f(x) is
     sufficiently close to 0."""
-    mid = (upper + lower) / 2.0
-    if abs(upper - lower) < epsilon:
-        return mid
-    elif f(upper) * f(lower) <= 0:
+    if f(upper) * f(lower) < 0:
         return find_root_in_range(lower, upper, f, epsilon)
     else:  # if same sign
         delta = upper - lower
@@ -77,10 +73,7 @@ def search_root_left(lower, upper, f, epsilon):
 def search_root_right(lower, upper, f, epsilon):
     """search to the right of upper to find an x value for which f(x) is
     sufficiently close to 0."""
-    mid = (upper + lower) / 2.0
-    if abs(upper - lower) < epsilon:
-        return mid
-    elif f(upper) * f(lower) <= 0:  # if different sign
+    if f(upper) * f(lower) < 0:  # if different sign
         return find_root_in_range(lower, upper, f, epsilon)
     else:
         delta = upper - lower
@@ -106,7 +99,6 @@ def factor_out_cubic_root(r, a, b, c):
     B = b + A * r
     C = c + B * r
     r2r3 = find_quadratic_roots(A, B, C)
-    print(f"quadratic roots: {r2r3=}")
     if r2r3:
         r2, r3 = r2r3
         return [r, r2, r3]
@@ -133,11 +125,9 @@ def find_cubic_roots(a, b, c, d):
         r = search_root_left(-1, 0, f, epsilon)
     else:
         r = search_root_right(0, 1, f, epsilon)
-    print(f" found cubic  root: {r=}")
     return factor_out_cubic_root(r, a, b, c)
 
 
-# print(cubic_roots(1, -6, 11, -6))
 
 def factor_out_quartic_root(r, a, b, c, d):
     A = a
@@ -157,10 +147,8 @@ def find_quartic_roots(a, b, c, d, e):
     #  = 4ax^3 + 3bx^2 + 2cx + d
     inflection_points = find_cubic_roots(4 * a, 3 * b, 2 * c, d)
     inflection_points.sort()
-    print(f"cubic roots: {inflection_points=}")
     f = make_quartic(a, b, c, d, e)
 
-    print(inflection_points)
     for i in [0, 1]:
         if f(inflection_points[i]) < 0 < f(inflection_points[i + 1]):
             r = find_root_in_range(inflection_points[i], inflection_points[i + 1], f, epsilon)
